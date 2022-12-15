698eb62202da66e0e34a69947ebc5f0e7e49aa86
Darx <mcrivabode@gmail.com>
2022-12-14 16:20:18 -0800

    set up seed.js

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -10,6 +10,29 @@ db.once('open', async () => {
         // delete all user and main data to avoid duplicates
         await User.deleteMany({});
         await Main.deleteMany({});
-        
+
+        // create all users
+        await User.create(userSeeds);
+
+        // iterate through all main seeds to create mains associated with specific users
+        for (let i = 0; i < mainSeeds.length; i++) {
+            const { _id, mainUser } = await Main.create(mainSeeds[i]);
+
+            const user = await User.findOneAndUpdate(
+                { username: mainUser },
+                {
+                    $addToSet: {
+                        mains: _id
+                    }
+                }
+            );
+        }
+    } catch (err) { // return error if seeds invalid
+        console.error(err);
+        process.exit(1);
     }
-})
+
+    // confirms seed implementation was successful and exit script
+    console.log('Seeds successfully planted.');
+    process.exit(1);
+});
